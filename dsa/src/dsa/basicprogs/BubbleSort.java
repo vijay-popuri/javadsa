bc63e5d8912fe8647dc0a284add753bd603085cb
vijay <vijaypopuri2021@gmail.com>
2025-01-09 22:53:31 +0530

    bubblesort

---
+++ dsa/src/dsa/basicprogs/BubbleSort.java
@@ -0,0 +1,20 @@
+package dsa.basicprogs;
+
+public class BubbleSort {
+	public static void main(String[] args) {
+		int a[]= {45,56,78,45,232,56,7,3,};
+		for(int i=0;i<a.length;i++) {
+			for(int j=i+1;j<a.length;j++) {
+				if(a[i]>a[j]) {
+					int temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+		for(int i=0;i<a.length;i++) {
+			System.out.print(a[i]+" ");
+		}
+	}
+
+}
